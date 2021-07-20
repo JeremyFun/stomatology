fe3f6ba9c5a4475afbd5034e4f55b372f9ddbd6f
Vladislav <getelementbyidkango@gmail.com>
2021-07-21 00:30:19 +0300

    untracked node_modules

--- backend/server.js
+++ backend/server.js
@@ -46,16 +46,16 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '/frontend/build')))
-//
-//     app.get('*', (req, res) =>
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
-// } else {
-//     app.get('/', (req, res) => {
-//         res.send('API is running')
-//     })
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running')
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
@@ -63,12 +63,12 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./frontend/build")));
-// Step 2:
-app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
-});
+// // Step 1:
+// app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+// // Step 2:
+// app.get("*", function (request, response) {
+//     response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+// });
 
 app.listen(PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
